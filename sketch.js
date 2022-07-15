b38324e6020e89b33947485ba912821015c5a926
piyushkoley <piyushkoley@gmail.com>
2022-07-15 14:16:30 +0530

    sound bar activity

---
+++ sketch.js
@@ -0,0 +1,26 @@
+var btnxyz = document.querySelectorAll(".btn");
+
+function stopSound(){
+    for(var i=0; i<btnxyz.length; i++) {
+        const sound = document.getElementById(btnxyz[i].innerText);
+        sound.pause();
+        sound.currentTime = 0;
+    }
+}
+
+function playSound(btn) {
+    btn.addEventListener("click", function(){
+        stopSound();
+        document.getElementById(btn.innerText).play();
+    })
+}
+
+for(var i=0; i<btnxyz.length; i++) {
+    var buttonxyz = btnxyz[i];
+    playSound(buttonxyz);
+    console.log(buttonxyz);
+} 
+
+document.querySelector(".stop").addEventListener("click", function(){
+    stopSound();
+});
